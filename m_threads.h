d9a022715457dc92c10bb0b415b563c81d5ade87
Vaphen <Igel101@web.de>
2014-04-06 14:15:28 +0200

    Create m_threads.h

---
+++ m_threads.h
@@ -0,0 +1,17 @@
+#ifndef __M_THREADS_H_INCLUDED_
+#define __M_THREADS_H_INCLUDED_
+
+#include "param.h"
+
+class m_threads : public Param{
+public:
+  m_threads();
+  ~m_threads();
+  void start_threads();
+private:
+  
+protected:
+  
+};
+
+#endif
